831d738637f9539c6c7e7d3276c10839ee75a4f9
Sergey Antipov <antip99999@mail.ru>
2022-12-25 15:16:17 +0300

    Fix(pizzasSlice): Add asyncThunk to pizzasSlice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import axios from 'axios';
 
 import { useSelector, useDispatch } from 'react-redux';
 
 import { setCategoryId, setSortType } from '../redux/slices/filterSlice';
-import { setPizzas } from '../redux/slices/pizzasSlice';
+import { fetchPizzas } from '../redux/slices/pizzasSlice';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import NotFoundPizzas from '../components/NotFoundPizzas';
 import { Skeleton } from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
@@ -18,7 +18,7 @@ function Home() {
 	const { categoryId, sortType, currentPage } = useSelector(
 		state => state.filter
 	);
-	const { pizzas } = useSelector(state => state.pizzas);
+	const { pizzas, status } = useSelector(state => state.pizzas);
 
 	const dispatch = useDispatch();
 
@@ -30,46 +30,12 @@ function Home() {
 		dispatch(setSortType(obj));
 	};
 
-	const [isLoading, setIsLoading] = React.useState(true);
-
 	React.useEffect(() => {
-		const getPizzas = async () => {
-			try {
-				const { data: pizzas } = await axios('http://localhost:3001/pizzas');
-				dispatch(setPizzas(pizzas));
-				setIsLoading(false);
-			} catch (error) {
-				setIsLoading(false);
-				alert('Ошибка при получении пицц!');
-				console.log('Error', error);
-			}
-		};
-
-		getPizzas();
-	}, []);
-
-	React.useEffect(() => {
-		const getPizzas = async () => {
-			const category = categoryId > 0 ? `category=${categoryId}` : '';
-			const sortBy = sortType.sortProperty.replace('-', '');
-			const orderBy = sortType.sortProperty.startsWith('-') ? 'desc' : 'asc';
-			const search = searchValue ? `title_like=${searchValue}` : '';
-
-			try {
-				const { data: pizzas } = await axios(
-					`http://localhost:3001/pizzas?${search}${category}&_sort=${sortBy}&_order=${orderBy}&_page=${currentPage}&_limit=4`
-				);
-
-				dispatch(setPizzas(pizzas));
-				setIsLoading(false);
-			} catch (error) {
-				setIsLoading(false);
-				alert('Ошибка при получении пицц!');
-				console.log('Error', error);
-			}
-		};
-		setIsLoading(true);
-		getPizzas();
+		const category = categoryId > 0 ? `category=${categoryId}` : '';
+		const sortBy = sortType.sortProperty.replace('-', '');
+		const orderBy = sortType.sortProperty.startsWith('-') ? 'desc' : 'asc';
+		const search = searchValue ? `title_like=${searchValue}` : '';
+		dispatch(fetchPizzas({ category, sortBy, orderBy, search, currentPage }));
 	}, [categoryId, sortType, searchValue, currentPage]);
 
 	return (
@@ -82,12 +48,17 @@ function Home() {
 					/>
 					<Sort value={sortType} setSortType={onSort} />
 				</div>
-				<h2 className='content__title'>Все пиццы</h2>
-				<div className='content__items'>
-					{isLoading
-						? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-						: pizzas.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
-				</div>
+				{status !== 'Error' && <h2 className='content__title'>Все пиццы</h2>}
+				{status === 'Error' ? (
+					<NotFoundPizzas />
+				) : (
+					<div className='content__items'>
+						{status === 'Loading'
+							? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+							: pizzas.map(pizza => <PizzaBlock key={pizza.id} {...pizza} />)}
+					</div>
+				)}
+
 				<div style={{ display: 'flex', justifyContent: 'center' }}>
 					<Pagination />
 				</div>
--- src/redux/slices/pizzasSlice.js
+++ src/redux/slices/pizzasSlice.js
@@ -1,7 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchPizzas = createAsyncThunk(
+	'pizzas/fetchPizzas',
+	async ({ category, sortBy, orderBy, search, currentPage }) => {
+		const { data: pizzas } = await axios(
+			`http://localhost:3001/pizzas?${search}${category}&_sort=${sortBy}&_order=${orderBy}&_page=${currentPage}&_limit=4`
+		);
+		return pizzas;
+	}
+);
 
 const initialState = {
 	pizzas: [],
+	status: 'pending',
 };
 
 export const pizzasSlice = createSlice({
@@ -11,6 +23,21 @@ export const pizzasSlice = createSlice({
 		setPizzas(state, action) {
 			state.pizzas = action.payload;
 		},
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(fetchPizzas.pending, state => {
+				state.pizzas = [];
+				state.status = 'Loading';
+			})
+			.addCase(fetchPizzas.fulfilled, (state, action) => {
+				state.pizzas = action.payload;
+				state.status = 'Success';
+			})
+			.addCase(fetchPizzas.rejected, state => {
+				state.pizzas = [];
+				state.status = 'Error';
+			});
 	},
 });
 
